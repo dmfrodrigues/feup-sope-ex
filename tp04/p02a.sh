aea9dfc2bb8188044f9be0a095b3b7cc19f521fb
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-03-10 21:12:58 +0000

    done tp04/p02

--- tp04/p02a.sh
+++ tp04/p02a.sh
@@ -1,3 +1,5 @@
 gcc -Wall p02a.c -o p02a
+./p01_kill.sh 1s -SIGINT ./p02a &
+./p01_kill.sh 2s -SIGINT ./p02a &
 ./p02a
 echo $?
--- tp04/p02b.sh
+++ tp04/p02b.sh
@@ -1,4 +1,5 @@
 gcc -Wall p02a.c -o p02a
 ./p01_kill.sh 1s -SIGTERM ./p02a &
+./p01_kill.sh 1s -SIGTERM ./p02a &
 ./p02a
 echo $?
