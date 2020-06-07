ee95718ce830dd2351b7751ee1fcc2c14c601218
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-08 00:01:27 +0100

    Solving c

---
+++ tp07/p05/c/shared_memory.h
@@ -0,0 +1,26 @@
+#ifndef SHARED_MEMORY_H_INCLUDED
+#define SHARED_MEMORY_H_INCLUDED
+
+#include "../queue.h"
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef struct {
+    queue_t buffer;
+    sem_t mutex;
+    sem_t empty;
+    sem_t full;
+} shared_memory_t;
+
+void shared_memory_init(shared_memory_t *mem);
+
+static const char* const MEM_NAME = "/mem1";
+
+#endif //SHARED_MEMORY_H_INCLUDED
