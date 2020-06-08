cbe27ae7511d0da3bc78f06bda4d744c05339884
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-09 00:25:20 +0100

    a

--- tp03/p09_a.sh
+++ tp03/p09_a.sh
@@ -5,6 +5,6 @@ set -e
 gcc -Wall p09_a.c -o p09_a
 ./p09_a .
 ./p09_a a
-./p09_a / > /dev/null &
-sleep 0.01s
-kill $(ps u | grep "ls -laR" | head -n 1 | awk '{print $2}')
+# ./p09_a / > /dev/null &
+# sleep 0.01s
+# kill $(ps u | grep "ls -laR" | head -n 1 | awk '{print $2}')
