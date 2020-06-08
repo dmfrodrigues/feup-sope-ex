2dfc0cc34e7638d0f3c2d8f165a3987ca070b899
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-09 00:22:05 +0100

    Changed test

--- tp03/p09_a.sh
+++ tp03/p09_a.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 set -e
 
 gcc -Wall p09_a.c -o p09_a
 ./p09_a .
 ./p09_a a
 ./p09_a / > /dev/null &
-sleep 1s
+sleep 0.01s
 kill $(ps u | grep "ls -laR" | head -n 1 | awk '{print $2}')
