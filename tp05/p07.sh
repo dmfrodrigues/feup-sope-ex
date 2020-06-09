e0b68af2936bb076b64e60321e1024a7a434a696
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-09 02:48:57 +0100

    Uncommented corrected issue

--- tp05/p07.sh
+++ tp05/p07.sh
@@ -2,6 +2,6 @@ set -e
 
 gcc -Wall p07_server.c -o p07_server
 gcc -Wall p07_client.c -o p07_client
-#./p07_server &
-#sleep 1
-#echo "7 3" | ./p07_client
+./p07_server &
+sleep 1
+echo "7 3" | ./p07_client
