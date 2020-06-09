c204d8e6bea7b3550d12b6096c59a8d0e299baa2
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-09 01:57:28 +0100

    Still trying to fix problem

--- tp04/kill.sh
+++ tp04/kill.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sleep $1
-psoriginal=$(ps u)
+psoriginal=$(ps aux)
 echo -e "$psoriginal"
-psresult=$(ps u | grep "$3" | grep -v "kill.sh")
+psresult=$(echo "$psoriginal" | grep "$3" | grep -v "kill.sh")
 proc=$(echo "$psresult" | head -n 1 | awk '{print $2}')
 name=$(echo "$psresult" | head -n 1 | awk '{for (i=11; i<=NF; i++) print $i}')
 echo Killing $proc \(\"$name\"\) with $2
