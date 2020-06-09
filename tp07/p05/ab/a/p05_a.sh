90f9dc06c9a0a63f5a4dbee7cb7f83f124f546c8
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-09 03:19:28 +0100

    Added shebangs, apparently they stopped my programs from giving random segfaults

--- tp07/p05/ab/a/p05_a.sh
+++ tp07/p05/ab/a/p05_a.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
+
 set -e
 
 gcc -Wall prod.c ../shared_memory.c ../../queue.c -lrt -pthread -o prod
 gcc -Wall cons.c ../shared_memory.c ../../queue.c -lrt -pthread -o cons
 
+sleep 1
 ./prod 10 100 &
 ./cons 10
 
 sleep 1
-
 ./prod 10 2 &
 ./cons 10
 
 sleep 1
-
 ./prod 10 1 &
 ./cons 10
--- tp07/p05/ab/b/p05_b.sh
+++ tp07/p05/ab/b/p05_b.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
+
 set -e
 
 gcc -Wall main.c ../shared_memory.c ../../queue.c -lrt -pthread -o main
 
+sleep 1
 ./main 10 100
 
 sleep 1
-
 ./main 10 2
 
 sleep 1
-
 ./main 10 1
