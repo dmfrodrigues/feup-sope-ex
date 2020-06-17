edb1c4b1eeaac224f862994dec2e154830452498
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-17 21:09:44 +0100

    Done with 6

--- exams/2018/2018N_06.c
+++ exams/2018/2018N_06.c
@@ -17,17 +17,19 @@ int process_dir(char *dirname) {
     if(!(dir = opendir(dirname))) return 1;
     while((entry = readdir(dir)) != NULL) {
         char path[1024];
-        // ‐‐‐ BLOCO A ‐‐‐
+        // --- BLOCO A ---
         sprintf(path, "%s/%s", dirname, entry->d_name);
         if(stat(path, &statbuf)) return 1;
-        if(S_ISDIR(statbuf.st_mode)) {  //se 'entry' for um diretório
+        if(S_ISDIR(statbuf.st_mode)) {  //se 'entry' for um diretorio
             /**
-             * This instruction is fundamental since it rejects the 'pseudofolders'
-             * `.` (current folder) and `..` (parent folder). Otherwise,
-             * `process_dir` would be called over and over again for the first
-             * folder and its successive parents.
+             * This instruction is fundamental since it rejects the
+             * 'pseudofolders' `.` (current folder) and
+             * `..` (parent folder).
+             * Otherwise, `process_dir` would be called over and over again
+             * for the first folder and its successive parents.
              */
-            if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            if(strcmp(entry->d_name, ".") == 0 ||
+               strcmp(entry->d_name, "..") == 0)
                 continue;
             pid_t pid = fork();
             if(pid == 0)/** Child */{
@@ -35,23 +37,26 @@ int process_dir(char *dirname) {
                 exit(0);
             } else if(pid < 0) return 1;
         }
-        // ‐‐‐ FIM DO BLOCO A ‐‐‐
-        // ‐‐‐ BLOCO B ‐‐‐
-        else if(S_ISREG(statbuf.st_mode)) {  //se 'entry' for um ficheiro regular
-            if(strstr(entry->d_name, filename) != NULL) {  //se o nome do ficheiro contiver filename
-                //cria um processo que invoca o utilitário 'cp'
+        // --- FIM DO BLOCO A ---
+        // --- BLOCO B ---
+        else if(S_ISREG(statbuf.st_mode)){//se 'entry' for ficheiro regular
+            //se o nome do ficheiro contiver filename
+            if(strstr(entry->d_name, filename) != NULL) {
+                //cria um processo que invoca o utilitario 'cp'
                 pid_t pid = fork();
                 if(pid == 0)/** Child */{
-                    if(execlp("cp", "cp", path, destination_dir, NULL)) return 1;
+                    if(execlp("cp", "cp", path, destination_dir, NULL))
+                        return 1;
                 } else if(pid < 0) return 1;                                       
             }
         }
-        // ‐‐‐ FIM DO BLOCO B ‐‐‐
+        // --- FIM DO BLOCO B ---
     }
     return 0;
 }
 
 // Usage: find_and_copy <dir_to_search> <filename> <destination_dir>
+// Example: ./find_and_copy 2018N_06_testdir/src file 2018N_06_testdir/dst
 int main(int argc, char *argv[]) {
     // 1)
     if(argc != 4) return 1;
@@ -63,6 +68,5 @@ int main(int argc, char *argv[]) {
     if(signal(SIGINT, SIG_IGN) == SIG_ERR) return 1;
     // 4)
     if(process_dir(dir_to_search)) return 1;
-
     return 0;
 }
--- exams/2018/2018N_06.sh
+++ exams/2018/2018N_06.sh
@@ -1,1 +1,2 @@
-gcc -Wall 2018N_06.c -o 2018N_06
+gcc -Wall 2018N_06.c -o find_and_copy
+./find_and_copy 2018N_06_testdir/src file 2018N_06_testdir/dst
