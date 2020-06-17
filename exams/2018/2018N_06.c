645bbf7946219b19d13e39df09f8fb580630bf41
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-17 19:05:09 +0100

    Some more fixes

--- exams/2018/2018N_06.c
+++ exams/2018/2018N_06.c
@@ -1,14 +1,14 @@
 int process_dir(char *dirname) {
     DIR *dir;
     struct dirent *entry;
-    struct stat statbuf;
+    // struct stat statbuf;
     if(!(dir = opendir(dirname))) return 1;
     while((entry = readdir(dir)) != NULL) {
         charpath[1024];
         // ‐‐‐ BLOCO A ‐‐‐
         ...;
         if(...) {  //se 'entry' for um diretório
-            if(strcmp(entry‐> d_name, ".") == 0 || strcmp(entry‐> d_name, "..") == 0)
+            if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
                 continue;
             ...;  //cria um processo que invoca process_dir()
         }
@@ -16,7 +16,7 @@ int process_dir(char *dirname) {
         // ‐‐‐ BLOCO B ‐‐‐
         else if(...) {  //se 'entry' for um ficheiro regular
             ...;
-            if(strstr(entry‐> d_name, filename) != NULL) {  //se o nome do ficheiro contiver filename
+            if(strstr(entry->d_name, filename) != NULL) {  //se o nome do ficheiro contiver filename
                 ...;                                        //cria um processo que invoca o utilitário 'cp'
             }
         }
