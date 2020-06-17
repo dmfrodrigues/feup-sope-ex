2abce0849b48ae5c625640e41d4d91b8ca6fc027
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-17 18:41:28 +0100

    Working on 2018 exam, exercise 6

---
+++ exams/2018/2018N_06.c
@@ -0,0 +1,26 @@
+int process_dir(char *dirname) {
+    DIR *dir;
+    struct dirent *entry;
+    struct stat statbuf;
+    if(!(dir = opendir(dirname))) return 1;
+    while((entry = readdir(dir)) != NULL) {
+        charpath[1024];
+        // ‐‐‐ BLOCO A ‐‐‐
+        ...;
+        if(...) {  //se 'entry' for um diretório
+            if(strcmp(entry‐> d_name, ".") == 0 || strcmp(entry‐> d_name, "..") == 0)
+                continue;
+            ...;  //cria um processo que invoca process_dir()
+        }
+        // ‐‐‐ FIM DO BLOCO A ‐‐‐
+        // ‐‐‐ BLOCO B ‐‐‐
+        else if(...) {  //se 'entry' for um ficheiro regular
+            ...;
+            if(strstr(entry‐> d_name, filename) != NULL) {  //se o nome do ficheiro contiver filename
+                ...;                                        //cria um processo que invoca o utilitário 'cp'
+            }
+        }
+        // ‐‐‐ FIM DO BLOCO B ‐‐‐
+    }
+    return0;
+}
