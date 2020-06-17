391928cf463c36837b7485573b35be934de03665
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-17 19:06:23 +0100

    Added main

--- exams/2018/2018N_06.c
+++ exams/2018/2018N_06.c
@@ -1,3 +1,12 @@
+#include <dirent.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+char *filename = NULL;
+char *destination_dir = NULL;
+
 int process_dir(char *dirname) {
     DIR *dir;
     struct dirent *entry;
@@ -22,5 +31,21 @@ int process_dir(char *dirname) {
         }
         // ‐‐‐ FIM DO BLOCO B ‐‐‐
     }
+    return 0;
+}
+
+// Usage: find_and_copy <dir_to_search> <filename> <destination_dir>
+int main(int argc, char *argv[]) {
+    // 1)
+    if(argc != 4) return 1;
+    // 2)
+    char *dir_to_search = argv[1];
+    filename = argv[2];
+    destination_dir = argv[3];
+    // 3)
+    if(signal(SIGINT, SIG_IGN) == SIG_ERR) return 1;
+    // 4)
+    if(process_dir(dir_to_search)) return 1;
+
     return 0;
 }
